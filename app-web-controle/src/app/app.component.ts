5b8a0f0b7b693e1f4a30fed55112f73da9573cc0
Valentin <valentin.renaudin@my-digital-school.org>
2024-02-20 11:25:45 +0100

    [ADD] Page List html/css

--- app-web-controle/src/app/app.component.ts
+++ app-web-controle/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-web-controle';
+
+  articleString : string = '';
+
+  addArticle() {
+    this.articleString = 'test';
+  }
+
 }
+
+// addArticle() {
+
+// }
